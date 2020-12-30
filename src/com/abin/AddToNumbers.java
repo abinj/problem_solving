3f7b0213fc46a8e6983ef9768654c4502bdb9828
abinj <abinmj656@gmail.com>
2020-12-30 14:42:50 -0500

    Add two numbers carry error fixed

--- src/com/abin/AddToNumbers.java
+++ src/com/abin/AddToNumbers.java
@@ -5,18 +5,22 @@ import java.util.LinkedList;
 public class AddToNumbers {
 
     public static void main(String[] args) {
-        int v1= addTwoNum(new ListNode(2), new ListNode(5)).val;
-        int v2 = addTwoNum(new ListNode(4), new ListNode(6)).val;
-        int v3 = addTwoNum(new ListNode(3), new ListNode(4)).val;
-        System.out.println(v1);
-        System.out.println(v2);
-        System.out.println(v3);
+        ListNode n1= addTwoNum(new ListNode(2), new ListNode(5), null);
+        ListNode n2 = addTwoNum(new ListNode(4), new ListNode(6), n1.next);
+        ListNode n3 = addTwoNum(new ListNode(3), new ListNode(4), n2.next);
+        System.out.println(n1.val);
+        System.out.println(n2.val);
+        System.out.println(n3.val);
+
     }
 
-    public static ListNode addTwoNum(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNum(ListNode l1, ListNode l2, ListNode carryNode) {
         ListNode dummyHead = new ListNode(0);
+        int carry =0;
+        if (carryNode != null) {
+            carry = carryNode.val;
+        }
         ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
         while (p != null || q != null) {
             int x = (p != null) ? p.val: 0;
             int y = (q != null) ? q.val:0;
